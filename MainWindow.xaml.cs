73172caef0c0aa27491a449ba5642c2058cefc45
admin174 <admin@chenk.ru>
2025-04-23 16:57:41 +0500

    заебало уже +

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using LykovFront.window;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.Json;
+
+namespace LykovFront
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            const string baseUrl = "http://localhost:5292/api";
+            HttpClient apiClient = new HttpClient();
+            var user = new AuthorizationeRequest { Login = loginTextBox.Text, Password = passwordTextbox.Text };
+            var json = JsonSerializer.Serialize(user);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var request = new HttpRequestMessage(HttpMethod.Get, $"{baseUrl}/User/authorize")
+            {
+                Content = content
+            };
+            var window = new reader();
+            window.Show();
+            this.Close();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
